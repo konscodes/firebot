776d0718fa520b024e608a259da638b4fe7471e6
Kons <katashi.rain@gmail.com>
2023-09-24 10:39:11 +0900

    Completed main functions

--- fire.py
+++ fire.py
@@ -10,7 +10,7 @@ app = firebase_admin.initialize_app()
 
 db = firestore.client()
 
-def get_rotation(chore: str) -> list[list]:
+def get_rotation(chore: str) -> (list[list], str):
     '''Get a list of rooms on rotation for a given chore
     
     Args:
@@ -18,41 +18,39 @@ def get_rotation(chore: str) -> list[list]:
 
     Returns:
         list[list]: a list of lists with room numbers for each team
+        str: start date of the rotation for further calculations
     '''
     chore_ref = db.collection('chores').document(chore)
 
     chore_snapshot = chore_ref.get(timeout=50000).to_dict()
+    
     try:
         chore_rotation = chore_snapshot['rotation']
     except TypeError:
         return 'Snapshot is empty. Chore name may be wrong'
     
+    try:
+        start_date = chore_snapshot['startDate']
+    except TypeError and KeyError:
+        start_date = 'Dec 25, 2022'
+        print('Start date was not found. Using default')
+
     rotation = [chore_rotation[f'{index}'] for index in range(len(chore_rotation))]
-    return rotation
+    return rotation, start_date
 
 
-def get_name(room: str) -> str:
-    '''Get the name of given room resident'''
-    room_ref = db.collection('users').document(room)
-    name = room_ref.get(timeout=50000).to_dict()['residentName']
-    return name
-
-
-def calculate_duty(rotation, interval='week', start_date='Jan 1, 2023') -> int:
+def calculate_duty(rotation, start_date, interval='week') -> int:
     '''Calculates who is on duty by extrapolating interval starting from a given date.
     
     Args:
         rotation(list[list]): a list of lists with room numbers for each team
         interval(str), optional: value to extrapolate over (weeks or months)
-        starting_date(str), optional: starting date of the rotation 
 
     Returns:
         tuple(index, [rooms]): an index and a list of room numbers on duty
     '''
-    start_datetime = datetime.strptime(start_date, '%b %d, %Y')
-    current_datetime = datetime.now()
 
-    difference = current_datetime - start_datetime
+    difference = datetime.now() - start_date
 
     weeks = difference.days // 7
     months = difference.days // 30
@@ -60,22 +58,50 @@ def calculate_duty(rotation, interval='week', start_date='Jan 1, 2023') -> int:
     print('Delta start and current:', difference.days, 'days')
     print('Weeks', weeks, 'Months', months)
 
-    if interval == 'week' and int(weeks==0):
-        return (0, rotation[0])
-    elif interval == 'week':
-        index = (weeks % len(rotation))
-        return (index, rotation[index])
+    def calculate_index(interval, rotation):
+        # account for less then a single interval from the starting date
+        if int(interval == 0):
+            return 0, rotation[0]
+        # the remainder of modula will correspond to an index in the rotation
+        index = (interval % len(rotation))
+        return index, rotation[index]
 
-    if interval == 'months' and int(weeks==0):
-        return (0, rotation[0])
+    if interval == 'week':
+        return calculate_index(weeks, rotation)
     elif interval == 'months':
-        index = (months % len(rotation))
-        return (index, rotation[index])
+        return calculate_index(months, rotation)
 
 
-#print(get_name('A101'))
-garbage_rotation = get_rotation('garbage')
+def get_names(rooms: list) -> tuple:
+    '''Get the names of given room residents
+    
+    Args:
+        rooms(list): a list of rooms to check
 
-rooms_on_duty = calculate_duty(garbage_rotation)
-#rooms = garbage_rotation[rooms_on_duty]
-print(rooms_on_duty)
+    Returns:
+        tuple(str): names of all residents in given rooms
+    '''
+    docs = db.collection('users').stream()
+    names = [doc.to_dict()['residentName'] for doc in docs if doc.id in rooms]
+    return names
+
+# data = {
+#     "startDate": datetime.strptime('Dec 26, 2022', '%b %d, %Y')
+#     }
+
+# db.collection("chores").document("garbage").set(data, merge=True)
+
+# garbage_rotation, start_date = get_rotation('garbage')
+# index, rooms_on_duty = calculate_duty(garbage_rotation, start_date)
+# names = get_names(rooms_on_duty)
+# print(rooms_on_duty)
+# print(names)
+
+chore_ref = db.collection('chores').document('groceries')
+
+chore_snapshot = chore_ref.get(timeout=50000).to_dict()
+
+print(chore_snapshot)
+
+# TODO 
+## add data to the db 
