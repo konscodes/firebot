19998e7a0109f2bfb8cd5b3d5ee5af03cd20c8f0
Kons <katashi.rain@gmail.com>
2023-09-26 18:02:10 +0900

    Added update function and fixed start date

--- fire.py
+++ fire.py
@@ -3,7 +3,7 @@ This script will perform the basic read and write on the database'''
 
 import firebase_admin
 from firebase_admin import firestore
-from datetime import datetime
+from datetime import datetime, timezone
 
 # Initialize the app with default credentials
 app = firebase_admin.initialize_app()
@@ -27,13 +27,19 @@ def get_rotation(chore: str) -> (list[list], str):
     try:
         chore_rotation = chore_snapshot['rotation']
     except TypeError:
-        return 'Snapshot is empty. Chore name may be wrong'
-    
+        return 'Snapshot is empty. Chore name may be wrong.'
+
+    default_date = datetime.strptime('2022-12-25 00:00:00', '%Y-%m-%d %H:%M:%S')
+
     try:
         start_date = chore_snapshot['startDate']
     except TypeError and KeyError:
-        start_date = 'Dec 25, 2022'
-        print('Start date was not found. Using default')
+        print("Error: Unable to retrieve a valid date. Using default.")
+        start_date = default_date
+
+    if not isinstance(start_date, datetime):
+        print("Error: The retrieved data is not a datetime object. Using default.")
+        start_date = default_date
 
     rotation = [chore_rotation[f'{index}'] for index in range(len(chore_rotation))]
     return rotation, start_date
@@ -50,7 +56,7 @@ def calculate_duty(rotation, start_date, interval='week') -> int:
         tuple(index, [rooms]): an index and a list of room numbers on duty
     '''
 
-    difference = datetime.now() - start_date
+    difference = datetime.now(tz=timezone.utc) - start_date
 
     weeks = difference.days // 7
     months = difference.days // 30
@@ -85,23 +91,21 @@ def get_names(rooms: list) -> tuple:
     names = [doc.to_dict()['residentName'] for doc in docs if doc.id in rooms]
     return names
 
-# data = {
-#     "startDate": datetime.strptime('Dec 26, 2022', '%b %d, %Y')
-#     }
 
-# db.collection("chores").document("garbage").set(data, merge=True)
+def update_data():
+    '''Generic function to update the start date in the db'''
+    data = {
+    "startDate": datetime.strptime('2022-12-25 00:00:00', '%Y-%m-%d %H:%M:%S')
+    }
+    
+    db.collection("chores").document("garbage").set(data, merge=True)
 
-# garbage_rotation, start_date = get_rotation('garbage')
-# index, rooms_on_duty = calculate_duty(garbage_rotation, start_date)
-# names = get_names(rooms_on_duty)
-# print(rooms_on_duty)
-# print(names)
 
-chore_ref = db.collection('chores').document('groceries')
-
-chore_snapshot = chore_ref.get(timeout=50000).to_dict()
-
-print(chore_snapshot)
+garbage_rotation, start_date = get_rotation('garbage')
+index, rooms_on_duty = calculate_duty(garbage_rotation, start_date)
+names = get_names(rooms_on_duty)
+print(index, rooms_on_duty)
+print(names)
 
 # TODO 
 ## add data to the db 
